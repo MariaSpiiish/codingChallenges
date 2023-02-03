9642ceb7ea1fb19063d28a7ef94da8fae2d03675
Maria Lapshina <mariaspiiish@gmail.com>
2023-02-03 16:10:40 +0400

    jewels and stones

---
+++ index.js
@@ -0,0 +1,40 @@
+// решение 1
+// данное на сайте яндекс контест с оберткой, которая мне непонятна
+
+const readline = require('readline');
+ 
+const rl = readline.createInterface({
+    input: process.stdin
+});
+ 
+let lines = [];
+rl.on('line', (line) => {
+    lines.push(line);
+}).on('close', () => {
+    const [jewels, stones] = lines
+    let result = 0;
+    for (let i = 0; i < stones.length; i++) {
+        if (jewels.includes(stones.charAt(i))) {
+            ++result;
+        }
+    }
+    process.stdout.write(result.toString());
+});
+
+//решение 2
+//в котором я просто перенесла решение 1 в мне понятный вид
+
+const a = 'fasdf';
+const c = 'fasdfasdf';
+
+function findJ(jewels, stones) {
+    let count = 0;
+    for (let i = 0; i < stones.length; i++) {
+        if(jewels.includes(stones.charAt(i))) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(findJ(a, c))
